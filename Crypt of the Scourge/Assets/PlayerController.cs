c31e7213b331267ff6b1c96f18334bebe19b35d9
DominoJP <66448483+DominoJP@users.noreply.github.com>
2022-09-22 12:48:44 -0700

    New code lesson
    Learned to add scripts

--- Crypt of the Scourge/Assets/PlayerController.cs
+++ Crypt of the Scourge/Assets/PlayerController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    int x;
+    int Add(int hours, int pay)
+    {
+        int result;
+        result = hours * pay;
+
+        return result;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello World");
+        x = Add(9, 5);
+        Debug.Log(x);
+  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
 }
