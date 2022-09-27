ec5895a4481a36f6f6ad9336b0c59b1fd8542cf0
DominoJP <66448483+DominoJP@users.noreply.github.com>
2022-09-27 12:57:51 -0700

    Player Movement script added
    Player movement added but not functional at current stage.

--- Crypt of the Scourge/Assets/PlayerController.cs
+++ Crypt of the Scourge/Assets/PlayerController.cs
@@ -4,28 +4,70 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    int x;
-    int Add(int hours, int pay)
-    {
-        int result;
-        result = hours * pay;
-
-        return result;
-    }
-
+    GameObject Player;
+    public float MoveSpeed;
+    Vector2 Movement = new Vector2();
+    Rigidbody2D RB;
+    
+    
     // Start is called before the first frame update
-    void Start()
+   void Start()
     {
-        Debug.Log("Hello World");
-        x = Add(9, 5);
-        Debug.Log(x);
-  
+     
+
+    }
+   
+    void Awake()
+    {
+        RB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Movement.x = 1 * MoveSpeed;
+            RB.velocity = Movement;
+            Debug.Log("Did we get here?");
+        }
+
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            Movement.x = -1 * MoveSpeed;
+            RB.velocity = Movement;
+            Debug.Log("Did we get ?");
+
+
+
+        }
+        else
+        {
+            Movement = new Vector2(0, Movement.y);
+            RB.velocity = Movement;
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Movement.y = 1 * MoveSpeed;
+            RB.velocity = Movement;
+            Debug.Log("Did we  here?");
+
+        }
+
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            Movement.y = -1 * MoveSpeed;
+            RB.velocity = Movement;  
+        }
+
+        else
+        {
+            Movement = new Vector2(Movement.x, 0);
+            RB.velocity = Movement;
+        }
     }
 
 }
