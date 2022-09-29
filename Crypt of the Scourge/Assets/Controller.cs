c2780448a19447e0adc0d7824019b3e4d0ca5bf8
DominoJP <joshjplano@gmail.com>
2022-09-29 08:58:32 -0700

    New Movement Implementation attempts
    Attempted to add a new movement implementation. Still experiencing no movement bug, problem may lie in project settings.

---
+++ Crypt of the Scourge/Assets/Controller.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Rigidbody2D RB;
+    public float Movespeed;
+    public float JumpSpeed;
+    Vector2 Movement = new Vector2();
+    public bool MidAir;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement = Input.GetAxisRaw("Horizontal");
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
